ba4b94172ea196e31626abc6b25909bb4db8ec4c
bdurb <brian@rpmgreenville.com>
2017-07-13 17:16:15 -0400

    first two cases pass on binarysearchtree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,6 +5,41 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+  insert(newValue) {
+    const node = new BinarySearchTree(newValue);
+    if (newValue <= this.value) {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.insert(newValue);
+      }
+    } else if (newValue > this.value) {
+      if (!this.right) {
+        this.right = node;
+      } else {
+        this.right.insert(newValue);
+      }
+    }
+  }
+  contains(newValue) {
+    let hasIt = false;
+    if (newValue === this.value) {
+      hasIt = true;
+    } else if (this.right !== null) {
+      if (this.right.contains(newValue)) {
+        hasIt = true;
+      }
+    } else if (this.left !== null) {
+      if (this.left.contains(newValue)) {
+        hasIt = true;
+      }
+    }
+    return hasIt;
+  }
+
+  depthFirstForEach() {
+
+  }
 }
 
 module.exports = BinarySearchTree;
