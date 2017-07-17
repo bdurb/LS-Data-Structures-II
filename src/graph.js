6c588bf7aed70e9c9a3a76ef5b20d4e0f0783ef3
bdurb <brian@rpmgreenville.com>
2017-07-16 20:42:00 -0400

    all tree and binary tree cases pass.  3 of graph pass

--- src/graph.js
+++ src/graph.js
@@ -1,7 +1,32 @@
 /* eslint-disable */
 class Graph {
   constructor() {
-    this.graph = {};
+    this.graph = {
+      node: [],
+      edge: []
+    }
+
+  }
+  addNode(newNode, toNode) {
+    this.graph.node.push(newNode);
+  }
+  contains(value) {
+    if (this.graph.node.includes(value)) {
+      return true;
+    }
+    return false;
+  }
+  removeNode(value) {
+      this.graph.node.splice(value);
+  }
+  addEdge() {
+
+  }
+  getEdge() {
+
+  }
+  removeEdge() {
+
   }
 }
 
--- src/tree.js
+++ src/tree.js
@@ -2,6 +2,24 @@ class Tree {
   constructor(value) {
     this.value = value;
     this.children = [];
+    this.storage = [value];
+  }
+  addChild(value) {
+    this.storage.push(value);
+    const node = new Tree(value);
+    this.children.push(node);
+  }
+  contains(newValue) {
+    if (this.value === newValue) {
+      return true;
+    } else if (this.children.length > 0) {
+      for (let i = 0; i < this.children.length; i++) {
+        if (this.children[i].contains(newValue)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
 
